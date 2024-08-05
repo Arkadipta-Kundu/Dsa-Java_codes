92a8356f085c6a166490c4ec7797655a08e2577d
Arkadipta-Kundu <arkadiptak49@gmail.com>
2024-08-05 12:23:23 +0530

    selection sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,6 +1,6 @@
 public class BubbleSort {
     public static void main(String args[]) {
-        int arr[] = { 1, 6, 5, 3, 8, 2, 4 };
+        int arr[] = { 1, 6, 5, 3, 8, 2, 4 , 9,25,19};
         
         // bubble sort
         for (int i = 0; i < arr.length - 1; i++) {
