9ff47068101ca093c94bb817bcd1573225581118
Arkadipta Kundu <108576784+Arkadipta-Kundu@users.noreply.github.com>
2024-08-05 09:42:16 +0530

    Delete java_progs directory

--- java_progs/corejava/strings.java
+++
@@ -1,9 +0,0 @@
-public class strings {
-    public static void main(String args[]) {
-        String s = "abcdefghijklmnopqrstuvwxyz";
-        for (int i = 0; i < s.length(); i++) {
-            System.out.print(s.charAt(i));
-            System.out.print(" ");
-        }
-    }
-}
